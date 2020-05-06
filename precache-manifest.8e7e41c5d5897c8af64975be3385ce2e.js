ea7a26634b0513c556755869cb6ee6ec7756afef
Amanda <plant.amnda@gmail.com>
2020-05-06 14:19:28 -0400

    Updates

---
+++ precache-manifest.8e7e41c5d5897c8af64975be3385ce2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a703e10eab721d4430675c187f7681f",
+    "url": "/EasyWeather/index.html"
+  },
+  {
+    "revision": "e58e9e02534d760d3e59",
+    "url": "/EasyWeather/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "661a97e5f082cbae5b83",
+    "url": "/EasyWeather/static/js/2.0580e049.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EasyWeather/static/js/2.0580e049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58e9e02534d760d3e59",
+    "url": "/EasyWeather/static/js/main.4967b491.chunk.js"
+  },
+  {
+    "revision": "fc93585e1a58865fba2c",
+    "url": "/EasyWeather/static/js/runtime-main.8b6620b2.js"
+  }
+]);
